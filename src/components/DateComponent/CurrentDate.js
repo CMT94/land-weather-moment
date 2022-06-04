4f97de53bb2ea5fb6cd73a1befa621f8768b1dcd
Clément MGT <cmt94.dev@gmail.com>
2022-06-04 15:38:05 +0200

    clean

--- src/components/DateComponent/CurrentDate.js
+++ src/components/DateComponent/CurrentDate.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { DateContainer } from "./DateStyles";
 
 const CurrentDate = () => {
--- src/components/TimeComponent/Time.js
+++ src/components/TimeComponent/Time.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import { FadeLoader } from "react-spinners";
 
 import { Welcome, TimeStamp, Greeting } from "./TimeStyles";
