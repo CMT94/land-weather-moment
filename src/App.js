5c90d08f5eb6bffd4e258828dacdd9df32cc1535
Clément MGT <cmt94.dev@gmail.com>
2022-06-04 08:56:57 +0200

    First API call w axios + npm i react-moment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Time from "./components/Time";
+import Weather from "./components/Weather";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <Weather />
+      <Time />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,30 @@
 import React from "react";
 import axios from "axios";
 
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=paris&units=metric&appid=31db298435df59aa92b1b72c2c7b27cd";
+
 const Weather = () => {
-  return <div></div>;
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  if (!data) return null;
+
+  return (
+    <div>
+      <p>{data.main.temp.toFixed(0)}</p>
+    </div>
+  );
 };
 
 export default Weather;
