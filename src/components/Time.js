931f68c2fabeeccb24c25eafe013d0f5f5d0d9d6
Clément MGT <cmt94.dev@gmail.com>
2022-06-04 09:09:24 +0200

    Init Time component w greeting func

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Moment from "react-moment";
+import "moment-timezone";
 
 const Time = () => {
-  return <div></div>;
+  let today = new Date();
+
+  let greeting = () => {
+    if (today.getHours() >= 5 && today.getHours() <= 12) {
+      return "Good Morning.";
+    } else if (today.getHours() >= 13 && today.getHours() <= 17) {
+      return "Good Afternoon.";
+    } else if (today.getHours() >= 18 && today.getHours() <= 4) {
+      return "Good Evening.";
+    } else {
+      return "Hi !";
+    }
+  };
+  return (
+    <div>
+      <p>
+        <Moment format="hh:mm"></Moment>
+        <p>{greeting()}</p>
+      </p>
+    </div>
+  );
 };
 
 export default Time;
